e0d484ced64ce32ff801c6cbdad64da1a39d8792
JJ <jiel0h.00@gmail.com>
2025-07-24 19:54:51 +0800

    test

---
+++ eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security/recommended',
+    'plugin:security-node/recommended',
+    'plugin:no-unsanitized/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'security',
+    'security-node',
+    'no-unsanitized'
+  ],
+  rules: {
+    'security/detect-eval-with-expression': 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
